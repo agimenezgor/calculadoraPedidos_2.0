691e6874da5a5235b8834beef018a0f8f20ec5de
agimenezgor <alvarogimenez88@gmail.com>
2021-02-10 13:45:48 +0100

    modifySupplier: implementado sin llamada a API

--- src/components/content/modifyData/modifySupplier/modifySupplier.js
+++ src/components/content/modifyData/modifySupplier/modifySupplier.js
@@ -1,11 +1,117 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
+import Card from 'react-bootstrap/Card';
+import { Redirect } from "react-router-dom";
+import { useForm } from "react-hook-form";
 
 function ModifySupplier() {
+
+    const { register, handleSubmit} = useForm();
+    const [calculateType, setCalculateType] = useState("");
     let number = parseInt(useParams().number.substring(1));
+    const [initialized, setInitialized] = useState(false);
+    const [validatedMessage, setValidatedMessage] = useState("");
+    
+    async function onSubmit (data, e) {
+      console.log(data)
+      /* await ValidateData(setValidatedMessage, data, e, setInitialized); */
+    }
+
+    function selected(e) {
+      setCalculateType(e.target.value)
+    }
+
     return (
-      <div>
-        this is the ModifySupplier page. Number of supplier: {number} 
+      <div className="bg-info pt-4">
+        <Card className="container" text="info" style={{borderRadius: "1em", minHeight:"60vh", minWidth:"40vw"}}>
+          <Card.Header>
+            <h2 className="d-flex justify-content-center p-4">Introduce los datos que deseas modificar y pulsa en guardar</h2>
+          </Card.Header>
+          <Card.Body className="d-flex justify-content-center">
+
+          <form onSubmit={handleSubmit(onSubmit)}>
+              <label className="d-flex justify-content-center mt-4">Introduce el nombre del proveedor</label>
+              <div className="d-flex justify-content-center mt-4">
+                  <input name="name" className="form-control" style={{minWidth:"45vw"}} ref={register()}/>
+              </div>
+
+              <div className="row d-flex justify-content-around">
+                  <div className="col-md-5">
+                      <label className="d-flex justify-content-center mt-4">Número de identificación</label>
+                      <div className="d-flex justify-content-center mt-4">
+                          <input name="number" type="number" className="form-control" style={{maxWidth:"15vw"}} ref={register()}/>
+                      </div>
+                  </div>
+                  <div className="col-md-5">
+                      <label className="d-flex justify-content-center mt-4">Días que tardan en servir</label>
+                      <div className="d-flex justify-content-center mt-4">
+                          <input name="days" type="number" className="form-control" style={{maxWidth:"15vw"}} ref={register()}/>
+                      </div>
+                  </div>   
+              </div>
+              
+              <label className="d-flex justify-content-center mt-4">Tipo de cálculo</label>
+              <div className="d-flex justify-content-center mt-4">
+                  <select className="form-select" name="calculateType"  ref={register()}>
+                      <option defaultValue disabled>Elige un tipo de cálculo</option>
+                      <option onClick={selected} value="Palets">Por número de palets</option>
+                      <option onClick={selected} value="Kilos">Por cantidad en kilos</option>
+                      <option onClick={selected} value="Franco">Por franco (€)</option>
+                  </select>
+              </div>
+
+              {calculateType === "Palets" ? (
+                <div className="row d-flex justify-content-around">
+                    <div className="col-md-5">
+                        <label className="d-flex justify-content-center mt-4">Cantidad mínima de palets</label>
+                        <div className="d-flex justify-content-center mt-4">
+                          <input name="minPalets" type="number" className="form-control"style={{maxWidth:"25vw"}} ref={register()}/>
+                        </div>
+                    </div>
+                    <div className="col-md-5">
+                        <label className="d-flex justify-content-center mt-4">Cantidad máxima de palets</label>
+                        <div className="d-flex justify-content-center mt-4">
+                          <input name="maxPalets" type="number" className="form-control" style={{maxWidth:"25vw"}} ref={register()}/>
+                        </div>
+                    </div>
+                </div>
+                
+              ): calculateType === "Kilos" ? (
+                  <div className="row d-flex justify-content-around">
+                    <div className="col-md-5">
+                        <label className="d-flex justify-content-center mt-4">Cantidad mínima de kilos</label>
+                        <div className="d-flex justify-content-center mt-4">
+                          <input name="minKilos" type="number" className="form-control" style={{maxWidth:"25vw"}} ref={register()}/>
+                        </div>
+                    </div>
+                    <div className="col-md-5">
+                        <label className="d-flex justify-content-center mt-4">Cantidad máxima de kilos</label>
+                        <div className="d-flex justify-content-center mt-4">
+                          <input name="maxKilos" type="number" className="form-control" style={{maxWidth:"25vw"}} ref={register()}/>
+                        </div>
+                    </div>
+                </div>
+
+              ): calculateType === "Franco" ? (
+                <div>
+                  <label className="d-flex justify-content-center mt-4">Franco mínimo</label>
+                    <div className="d-flex justify-content-center mt-4">
+                      <input name="money" type="number" className="form-control" style={{maxWidth:"25vw"}} ref={register()}/>
+                    </div>
+                </div>
+              ): (
+                <div></div>
+              )}
+              <div className="d-flex justify-content-center">
+                <button className="btn btn-outline-info mt-5">Guardar</button>
+              </div>
+            </form>
+            {initialized === true ? (
+              <Redirect to="/proveedores"/>
+            ): (<span></span>)}
+          </Card.Body>
+          {/* <PrintValidatedMessage message={validatedMessage}/> */}
+        </Card>
       </div>
     );
   }
