20e9ef386b6d24cc0bd9420f2b2f747db9fdf74f
agimenezgor <alvarogimenez88@gmail.com>
2021-02-11 13:17:18 +0100

    removeSupplier: implementado si llamada a API

--- src/components/content/modifyData/removeSupplier/removeSupplier.js
+++ src/components/content/modifyData/removeSupplier/removeSupplier.js
@@ -1,9 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Redirect } from 'react-router-dom';
+import swal from 'sweetalert';
 
 function RemoveSupplier() {
+  const [redirecting, setRedirecting] = useState(false);
+    const showAlert = () => {
+      swal({
+        title: "Borrar proveedor",
+        text: "Estás seguro de que deseas borrar el proveedor XXX??",
+        icon: "warning",
+        buttons: ["Cancelar", "Borrar"]
+      })
+      .then(response => {
+        if(!response){
+          swal({
+            title: "Cancelado",
+            text: "Operación cancelada correctamente",
+            icon: "success",
+            button: {
+              text: "Aceptar",
+            }
+          })
+          .then(() => {setRedirecting(true)})
+        }
+        else{
+          swal({
+            title: "Borrado",
+            text: "Proveedor borrado correctamente",
+            icon: "success",
+            button: "Aceptar"
+          })
+          .then(() => {setRedirecting(true)})
+        }
+      })
+    }
     return (
       <div>
-        this is the RemoveSupplier page
+        {redirecting === false ? showAlert(): (<span></span>)}
+        {redirecting === true ? (<Redirect to="/proveedores"/>): (<span></span>)}
       </div>
     );
   }
