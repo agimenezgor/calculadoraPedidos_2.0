2dd78b5618d3467b7ed147e81e0213e4e41b3516
agimenezgor <alvarogimenez88@gmail.com>
2021-02-11 13:32:00 +0100

    removeSupplier: número de proveedor a borrar

--- src/components/content/modifyData/removeSupplier/removeSupplier.js
+++ src/components/content/modifyData/removeSupplier/removeSupplier.js
@@ -1,13 +1,14 @@
 import React, { useState } from 'react';
-import { Redirect } from 'react-router-dom';
+import { Redirect, useParams } from 'react-router-dom';
 import swal from 'sweetalert';
 
 function RemoveSupplier() {
-  const [redirecting, setRedirecting] = useState(false);
+    const [redirecting, setRedirecting] = useState(false);
+    const number = useParams().number.substring(1)
     const showAlert = () => {
       swal({
         title: "Borrar proveedor",
-        text: "Estás seguro de que deseas borrar el proveedor XXX??",
+        text: `Estás seguro de que deseas borrar el proveedor número ${number}?`,
         icon: "warning",
         buttons: ["Cancelar", "Borrar"]
       })
@@ -15,7 +16,7 @@ function RemoveSupplier() {
         if(!response){
           swal({
             title: "Cancelado",
-            text: "Operación cancelada correctamente",
+            text: "Operación cancelada",
             icon: "success",
             button: {
               text: "Aceptar",
