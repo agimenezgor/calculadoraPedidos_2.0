cf66ca5927301e87723f15298009e80edc380374
agimenezgor <alvarogimenez88@gmail.com>
2021-02-12 11:52:54 +0100

    Print-References: comprobamos si hay usuario

--- src/components/content/printReferences.js
+++ src/components/content/printReferences.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import Cookies from 'universal-cookie';
 
 function PrintReferences(props) {
+    // Primero comprobamos hay un usuario inicializado mediante las cookies.
+        // Si no lo está, pintamos las referencias de muestra.
+        // Si lo está, hacemos la llamada a la API.
+    const cookies = new Cookies();
+    let userInitialized = false;
+    let disabledButtons = true;
+
+    if(cookies.get('name') !== undefined){
+        userInitialized = true;
+        disabledButtons = false;
+    }
+    
     return (
       <div>
-        this is the print references page. Supplier number: {props.number}
+          {userInitialized === true ? (
+          <div>
+              Usuario inicializado
+              </div>) 
+          :(<div>
+              No hay ningún usuario inicializado
+            </div>)}
+        Número de proveedor: {props.number}
       </div>
     );
   }
