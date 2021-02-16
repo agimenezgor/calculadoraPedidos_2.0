268c6eead5364b55ec30e4d00a848d0065407ae1
agimenezgor <alvarogimenez88@gmail.com>
2021-02-16 13:35:28 +0100

    coockie consent: implementado en app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Footer from './components/footer/footer';
 import "video-react/dist/video-react.css";
 import {useState} from 'react';
 import Cookies from 'universal-cookie';
+import CookieConsent from "react-cookie-consent";
 
 function App() {
   const [user, setUser] = useState("");
@@ -26,6 +27,26 @@ function App() {
       <footer className="App-footer">
           <Footer/>
       </footer>
+      <CookieConsent
+          location="bottom"
+          style={{
+            background: "linear-gradient(to right, #abb2b9, #34495e)",
+            textAlign: "center"
+          }}
+          buttonText="Acepto"
+          buttonStyle={{
+            background:"#d4ac0d",
+            borderRadius: "0.3em",
+            color: "#34495e",
+            paddingLeft: "0.8em",
+            paddingRight: "0.8em" 
+          }}
+          containerClasses="alert alert-warning col-lg-12"
+          overlay
+        >
+          Este sitio web utiliza cookies para mejorar la experiencia del usuario.{" "}
+          <div style={{ fontSize: "0.8em" }}>Si permaneces en la web, aceptas el uso de todas las cookies</div>
+      </CookieConsent>
     </div>
   );
 }
