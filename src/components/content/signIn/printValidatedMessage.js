7a4247b67ebf1991b07c7df17b16c5dea0cee58e
agimenezgor <alvarogimenez88@gmail.com>
2021-02-16 11:58:32 +0100

    signIn: alerta actualizada

--- src/components/content/signIn/printValidatedMessage.js
+++ src/components/content/signIn/printValidatedMessage.js
@@ -1,16 +1,47 @@
 import React from 'react';
+import swal from 'sweetalert';
 
 function PrintValidatedMessage(message){
     const validatedMessage = message.message;
+    function successAlert() {
+        swal({
+          title: "Conectado",
+          text: "Sesión iniciada correctamente",
+          icon: "success",
+          button: {
+              text: "Aceptar",
+          }
+          })
+      }
+      function warningPassword() {
+        swal({
+          title: "Atención!",
+          text: "La contraseña es incorrecta",
+          icon: "warning",
+          button: {
+              text: "Aceptar",
+          }
+          })
+      }
+      function errorUser() {
+        swal({
+          title: "Error",
+          text: "El usuario no existe en la base de datos",
+          icon: "error",
+          button: {
+              text: "Aceptar",
+          }
+          })
+      }
     return <div>
             {validatedMessage === "" ? (
                 <span></span>)
               : validatedMessage === "Sesión iniciada correctamente" ? (
-              <div className="bg-success text-white d-flex justify-content-center mt-4 p-4"><h4>{validatedMessage}</h4></div>)
+                    successAlert())
               : validatedMessage === "La contraseña es incorrecta" ? (
-                  <div className="bg-warning text-white d-flex justify-content-center mt-4 p-4"><h4>{validatedMessage}</h4></div>
+                    warningPassword()
               ): validatedMessage === "El usuario no existe en la base de datos" ?(
-                  <div className="bg-danger text-white d-flex justify-content-center mt-4 p-4"><h4>{validatedMessage}</h4></div>
+                  errorUser()
               ): (<span></span>)}
             </div>
 }
