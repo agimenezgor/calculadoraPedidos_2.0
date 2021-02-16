d5e005d1a3123536b032d801045c9d87c350f97b
agimenezgor <alvarogimenez88@gmail.com>
2021-02-16 11:53:22 +0100

    modifySupplier, newReference: alertas cambiadas

--- src/components/content/modifyData/modifySupplier/printValidatedMessage.js
+++ src/components/content/modifyData/modifySupplier/printValidatedMessage.js
@@ -1,16 +1,34 @@
 import React from 'react';
-import Alert from 'react-bootstrap/Alert'
+import swal from 'sweetalert';
 
 function PrintValidatedMessage(message){
     const validatedMessage = message.message;
+    function successAlert(){
+      swal({
+        title: "Modificado",
+        text: "Proveedor modificado correctamente",
+        icon: "success",
+        timer: 2000,
+        button: {
+          text: "Aceptar",
+        }
+      })
+    }
+    function dangerAlert(){
+      swal({
+        title: "Error",
+        text: "No podemos actualizar el proveedor. Inténtalo más tarde",
+        icon: "error",
+        button: "Aceptar",
+        timer: 2000
+      })
+    }
     return <div>
             {validatedMessage === "" ? (
                 <span></span>)
               : validatedMessage === "Proveedor modificado correctamente" ? (
-                <Alert variant="success" className="d-flex justify-content-center">
-                  Proveedor modificado correctamente!
-                </Alert>
-              ): (<span></span>)}
+                successAlert()
+              ): (dangerAlert())}
             </div>
 }
 export default PrintValidatedMessage;
--- src/components/content/modifyData/newReference/printValidateMessage.js
+++ src/components/content/modifyData/newReference/printValidateMessage.js
@@ -1,23 +1,47 @@
 import React from 'react';
-import Alert from 'react-bootstrap/Alert'
+import swal from 'sweetalert';
 
 function PrintValidatedMessage(message){
     const validatedMessage = message.message;
+    function successAlert() {
+      swal({
+        title: "Guardado",
+        text: "Referencia guardada correctamente",
+        icon: "success",
+        button: {
+            text: "Aceptar",
+        }
+        })
+    }
+    function dangerNumber() {
+      swal({
+        title: "Error",
+        text: "El número de referencia ya existe en la base de datos",
+        icon: "error",
+        button: {
+            text: "Aceptar",
+        }
+        })
+    }
+    function dangerServer() {
+      swal({
+        title: "Error",
+        text: "Imposible conectar con el servidor. Inténtelo de nuevo más tarde",
+        icon: "error",
+        button: {
+            text: "Aceptar",
+        }
+        })
+    }
     return <div>
             {validatedMessage === "" ? (
                 <span></span>)
               : validatedMessage === "Referencia guardada correctamente" ? (
-                <Alert variant="success" className="d-flex justify-content-center">
-                  Referencia registrada correctamente!
-                </Alert>
+                successAlert()
               ): validatedMessage ==="El número de referencia ya existe en la base de datos" ? (
-                <Alert variant="danger" className="d-flex justify-content-center">
-                  El número de referencia ya existe en la base de datos
-                </Alert>
+                dangerNumber()
               ): validatedMessage ==="Imposible conectar con el servidor. Inténtelo de nuevo más tarde" ? (
-                <Alert variant="danger" className="d-flex justify-content-center">
-                  Imposible conectar con el servidor. Inténtelo de nuevo más tarde
-                </Alert>
+                dangerServer()
               ): (<span></span>)}
             </div>
 }
