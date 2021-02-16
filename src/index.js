fdb19cb45ee32c894ede47770e8f914c0736720f
agimenezgor <alvarogimenez88@gmail.com>
2021-02-16 14:04:24 +0100

    fav icon actualizado

--- src/index.js
+++ src/index.js
@@ -3,9 +3,12 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
+import Favicon from 'react-favicon';
+import calc from './components/img/calc2.png'
 
 ReactDOM.render(
   <BrowserRouter>
+      <Favicon url={calc} />
       <App />
   </BrowserRouter>,
   document.getElementById('root')
